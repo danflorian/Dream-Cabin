d72f8a22123755528c9c8a818904b5c52c6d040b
Dan Panait <39486974+danflorian@users.noreply.github.com>
2019-11-20 17:49:46 +0200

    Add files via upload

--- shared.js
+++ shared.js
@@ -4,7 +4,6 @@ var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
-var ctaButton = document.querySelector(".main-nav__item--cta");
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function() {
@@ -43,14 +42,3 @@ toggleButton.addEventListener("click", function() {
   }, 10);
 });
 
-ctaButton.addEventListener('animationstart', function(event) {
-  console.log('Animation started', event);
-})
-
-ctaButton.addEventListener('animationend', function(event) {
-  console.log('Animation ended', event);
-})
-
-ctaButton.addEventListener('animationiteration', function(event) {
-  console.log('Animation iteration', event);
-})
